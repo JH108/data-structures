9a99558655b97cbcee579034f2a17e0e45fb0a80
Jesse Hill <jsnahill@gmail.com>
2017-02-08 15:10:41 -0800

    Implement functions for Queue to pass all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,20 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var keys = Object.keys(storage);
+    var nextKey  = keys.length ? keys[keys.length - 1] + 1 : 0;
+    storage[nextKey] = value;
   };
 
   someInstance.dequeue = function() {
+    var keys = Object.keys(storage);
+    var deletedValue = storage[keys[0]];
+    delete storage[keys[0]];
+    return deletedValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
