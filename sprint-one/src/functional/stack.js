a8d02c5a4d8b7dd04f8fd5b7ff460552ed3e9f01
Jesse Hill <jsnahill@gmail.com>
2017-02-08 15:45:51 -0800

    Implement functions to make all tests pass for functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,22 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+
   someInstance.push = function(value) {
+    var keys = Object.keys(storage);
+    var nextKey = keys.length ? keys[keys.length - 1] + 1 : 0;
+    storage[nextKey] = value;
   };
 
   someInstance.pop = function() {
+    var keys = Object.keys(storage);
+    var lastKeyValue = storage[keys[keys.length - 1]];
+    delete storage[keys[keys.length - 1]];
+    return lastKeyValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
