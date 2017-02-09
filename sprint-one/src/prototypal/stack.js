4c17a0e32b3349541a7d93c08ca23e4f4d0fbfa0
Jesse Hill <jsnahill@gmail.com>
2017-02-09 10:02:41 -0800

    Implement stack data structure prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  // newStack.sizeA = 0;
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  var keys = Object.keys(this);
+  var nextKey = keys.length ? keys[keys.length - 1] + 1 : 0;
+  this[nextKey] = value;
+};
+
+stackMethods.pop = function() {
+  var keys = Object.keys(this);
+  var lastKeyValue = this[keys[keys.length - 1]];
+  delete this[keys[keys.length - 1]];
+  return lastKeyValue;
+};
+
+stackMethods.size = function() {
+  return Object.keys(this).length;
+};
 
