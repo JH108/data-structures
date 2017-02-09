8a45c3021be0a4b8eb5f51d13e21f81ca351f3ac
Jesse Hill <jsnahill@gmail.com>
2017-02-08 20:15:04 -0800

    Implement functions for functional-shared queue data structure

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.sizeA = 0;
+  newQueue.lastKey = 0;
+  _.extend(newQueue, queueMethods);
+  return newQueue;
 };
 
 var queueMethods = {};
 
 
+queueMethods.enqueue = function(value) {
+  var keys = Object.keys(this);
+  var nextKey = this.lastKey ? this.lastKey + 1 : 1;
+  this[nextKey] = value;
+  this.sizeA++;
+  this.lastKey = nextKey;
+};
+
+queueMethods.dequeue = function() {
+  if (this.sizeA) {
+    var keys = Object.keys(this);
+    var deletedValue = this[keys[0]];
+    delete this[keys[0]];
+    this.sizeA--;
+    return deletedValue;
+  }
+};
+
+queueMethods.size = function() {
+  return this.sizeA;
+};
