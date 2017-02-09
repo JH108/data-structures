5b9eeaf6d52dbfd2d0f9b6033511753002db68e2
Jesse Hill <jsnahill@gmail.com>
2017-02-09 10:24:33 -0800

    Implement stack data structure using pseudoclassical instantiation

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not not reference your old code in writing the newyle.
+
+  if (arguments.length > 0) {
+    this.args = arguments[0];
+  }
 };
 
+Stack.prototype.push = function(value) {
+  var keys = Object.keys(this);
+  var nextKey = keys.length ? keys[keys.length - 1] + 1 : 0;
+  this[nextKey] = value;
+};
 
+Stack.prototype.pop = function() {
+  var keys = Object.keys(this);
+  var lastKeyValue = this[keys[keys.length - 1]];
+  delete this[keys[keys.length - 1]];
+  return lastKeyValue;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this).length;
+};
