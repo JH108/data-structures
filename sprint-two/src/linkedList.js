350b11a2f7e37ab0850e1d8bcfaf315ffef1d7e5
Jesse Hill <jsnahill@gmail.com>
2017-02-09 16:12:30 -0800

    Implement first version of linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,28 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    list.tail = new Node(value);
+    if(list.head === null) {
+      list.head = list.tail;
+    } else {
+      list.head.next = list.tail.value;
+    }
   };
 
   list.removeHead = function() {
+    var valueToBeDeleted = list.head.value;
+    list.head.value = list.head.next;
+    list.head.next = null;
+    return valueToBeDeleted;
   };
 
   list.contains = function(target) {
+    for(var key in list.head) {
+      if(list.head[key] === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -23,6 +39,7 @@ var Node = function(value) {
 
   return node;
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
