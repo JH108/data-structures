6e9ca7193cb6b4b966b8e2ea644ed5fa65b349a8
Jesse Hill <jsnahill@gmail.com>
2017-02-10 09:51:39 -0800

    Implement tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,20 +3,41 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
+// tree.children[0].addChild(6);
+// expect(tree.children[0].children[0].value).to.equal(6);
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // if (this.children === undefined) {
+  //   this.children = {};
+  // }
+  //this.children.push({value: value, children : [], addChild: treeMethods.addChild});
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var targetFound = false;
+
+  function recurseOverChildren(node) {
+    for(var i = 0; i < node.length; i++) {
+      if(node[i].value === target) {
+        targetFound = true;
+        return targetFound;
+      } else if(node[i].children.length > 0) {
+        recurseOverChildren(node[i].children);
+      }
+    }
+  }
+  recurseOverChildren(this.children);
+  return targetFound;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
